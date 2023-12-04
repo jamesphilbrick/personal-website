5013639e84a754a5b6ee0ffb54a8921ef7a920b9
James Philbrick <63430438+jamesphilbrick@users.noreply.github.com>
2023-12-04 18:48:49 +0000

    Add files via upload
    Re-style initial upload

---
+++ pages/blog.md
@@ -0,0 +1,7 @@
+[Home](https://jamesphilbrick.github.io/personal-website/)
+# Blog ✍
+
+Below are some incoherent writings on some things I find interesting, and that which you might too: 
+
+* [Distance visualisation - how far am I from running to NYC?](https://jamesphilbrick.github.io/personal-website/pages/running-leeds-to-nyc/running-leeds-to-nyc.html)
+* [My newly adopted folder naming convention - wow, how interesting...](https://jamesphilbrick.github.io/personal-website/pages/file-structure-conventions/file-structure-conventions.html)
--- scripts/header.js
+++ scripts/header.js
@@ -1,10 +1,15 @@
 const headerTemplate = document.createElement('template');
 headerTemplate.innerHTML = `
-	<div class="header">
-			<a class="home" href="index.html">Home</a>
-			<a href="contact.html">Contact</a>
-			<a href="about.html">About</a>
-			<a class="blog" href="blog.html">Blog</a>
+<div class="container">
+	<div class="header-name-title">
+		<img src="resources/images/temp_title_name.jpg" alt="James Philbrick" class="header-name-title-image"></img>
 	</div>
+	<div class="header">
+		<a class="home" href="/index.html">Home</a>
+		<a href="/pages/contact.html">Contact</a>
+		<a href="/pages/about.html">About</a>
+		<a class="blog" href="/pages/blog.html">Blog</a>
+	</div>
+<div>
 `;
 document.body.appendChild(headerTemplate.content);
