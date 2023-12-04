364635ce7d0b5d4e257027d3beb477fbaa1a63de
James Philbrick <63430438+jamesphilbrick@users.noreply.github.com>
2023-12-04 18:53:17 +0000

    Update header.js
    Fixed links

--- scripts/header.js
+++ scripts/header.js
@@ -5,10 +5,10 @@ headerTemplate.innerHTML = `
 		<img src="resources/images/temp_title_name.jpg" alt="James Philbrick" class="header-name-title-image"></img>
 	</div>
 	<div class="header">
-		<a class="home" href="/index.html">Home</a>
-		<a href="/pages/contact.html">Contact</a>
-		<a href="/pages/about.html">About</a>
-		<a class="blog" href="/pages/blog.html">Blog</a>
+		<a class="home" href="/personal-website/index.html">Home</a>
+		<a href="/personal-website/pages/contact.html">Contact</a>
+		<a href="/personal-website/pages/about.html">About</a>
+		<a class="blog" href="/personal-website/pages/blog.html">Blog</a>
 	</div>
 <div>
 `;
